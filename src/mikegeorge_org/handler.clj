9029e7aeed1509a818e6d57d98f602633a29c3ab
Mike George <mikeyg6754@gmail.com>
2013-09-24 17:41:46 +0000

    Initial commit

---
+++ src/mikegeorge_org/handler.clj
@@ -0,0 +1,51 @@
+(ns mikegeorge-org.handler  
+  (:require [compojure.core :refer [defroutes]]            
+            [mikegeorge-org.routes.home :refer [home-routes]]
+            [noir.util.middleware :as middleware]
+            [compojure.route :as route]
+            [taoensso.timbre :as timbre]
+            [com.postspectacular.rotor :as rotor]))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(defn init
+  "init will be called once when
+   app is deployed as a servlet on
+   an app server such as Tomcat
+   put any initialization code here"
+  []
+  (timbre/set-config!
+    [:appenders :rotor]
+    {:min-level :info
+     :enabled? true
+     :async? false ; should be always false for rotor
+     :max-message-per-msecs nil
+     :fn rotor/append})
+  
+  (timbre/set-config!
+    [:shared-appender-config :rotor]
+    {:path "mikegeorge_org.log" :max-size (* 512 1024) :backlog 10})
+  
+  (timbre/info "mikegeorge-org started successfully"))
+
+(defn destroy
+  "destroy will be called when your application
+   shuts down, put any clean up code here"
+  []
+  (timbre/info "mikegeorge-org is shutting down..."))
+
+(def app (middleware/app-handler
+           ;; add your application routes here
+           [home-routes app-routes]
+           ;; add custom middleware here
+           :middleware []
+           ;; add access rules here
+           :access-rules []
+           ;; serialize/deserialize the following data formats
+           ;; available formats:
+           ;; :json :json-kw :yaml :yaml-kw :edn :yaml-in-html
+           :formats [:json-kw :edn]))
+
+(def war-handler (middleware/war-handler app))
