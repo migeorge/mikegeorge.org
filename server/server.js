99bdb66de36522ae64d9285b2e1144ca2fb347c8
Mike George <mike@tallduck.com>
2015-02-13 15:07:34 -0500

    order skills by most confident

--- server/server.js
+++ server/server.js
@@ -2,8 +2,8 @@ Meteor.publish("jobs", function () {
 	return Jobs.find();
 });
 
-Meteor.publish("skills", function () {		
-	return Skills.find();
+Meteor.publish("skills", function () {
+	return Skills.find({}, {sort: {confidence: -1}});
 });
 
 Meteor.publish("schools", function () {
